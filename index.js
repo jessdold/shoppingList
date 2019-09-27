897a706964662ff436b24fb43e00f16f60275dee
Jess Dold <jessica.dold6@gmail.com>
2019-09-26 20:18:27 -0400

    fixed buttons

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
   //function to add item to the list
 
-  $('js-shopping-list-form').on('submit', function(event) {
+  $('#js-shopping-list-form').on('submit', function(event) {
     
+    event.preventDefault();
     //grab the value of what got typed in the form
     var item = $('#shopping-list-entry').val();
     // insert list item with that value
-    $('shopping-list').append(`<li>
+    $('.shopping-list').append(`<li>
         <span class="shopping-item">${item}</span>
         <div class="shopping-item-controls">
           <button class="shopping-item-toggle">
@@ -17,20 +18,18 @@
         </div>
       </li>`);
     $('#shopping-list-entry').val('');
-    event.preventDefault();
+    
   })
 
   //function to line-through or undo line-through when check is pushed
-$('.shopping-item-toggle').on('submit', function () {
-  // if the span with the '.shopping-item' class also includes '.shopping-item__checked' class, remove the checked class. else add checked class to that span
-  $(".shopping-item").toggleClass(".shopping-item__checked")
-})
+  $('.shopping-item-toggle').on('click', function () {
+    $(this).parents('li').find('.shopping-item').toggleClass("shopping-item__checked")
+  })
 
 // function to delete a list item when delete is pushed
 
-$('.shopping-item-delete').on('submit', 'li', function(event) {
+$('.shopping-item-delete').on('submit', function(event) {
   // delete that whole <li>...but how do i tell it to grab/delete only that particular li?
-  $(this).remove();
-  event.preventDefault();
+  $(this).parents("li").remove();
 })
 
